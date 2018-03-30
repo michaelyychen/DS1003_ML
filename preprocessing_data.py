fed7ec4ce8834a0209215f0d5d12dfae7f7b01b9
Edison Huang <a123j2010@gmail.com>
2018-03-29 20:26:31 -0400

    add preprocessing

---
+++ preprocessing_data.py
@@ -0,0 +1,118 @@
+import random
+import numpy as np
+import pandas as pd
+import pickle
+
+def load_problem(file_name = "data.pickle"):
+    f_myfile = open(file_name, 'rb')
+    data = pickle.load(f_myfile)
+    f_myfile.close()
+    return data["x_train"], data["y_train"],data["x_test"], data["y_test"]
+
+def read_and_combine_data(files, features):
+    frames = []
+    for f in files:
+        file = pd.read_csv(f)[features]
+        frames.append(file)
+    return pd.concat(frames).fillna("-")
+
+def save_as_picke(data,filename = "data.pickle"):
+    with open(filename, 'wb') as outfile:
+        pickle.dump(data, outfile, pickle.HIGHEST_PROTOCOL)
+def age_conversion(p):
+    p["dob"] = pd.to_datetime(p["dob"])
+    temp = pd.DataFrame({'year': [1980],
+                        'month': [1],
+                        'day': [1]})
+    temp = pd.to_datetime(temp)
+
+    p["dob"] = p["dob"].fillna(temp)
+    p["age"] = pd.Timestamp('today')
+    p["age"] = (p["age"] - p["dob"])/ np.timedelta64(1, 'Y')
+    return p
+def union_batter_pitcher(p,f):
+    #last first throws bats height weight dob
+    p = p.copy()
+    p.rename(columns={'bref_id': 'pitcher', 'last': 'p_last', 'first':'p_first', 'height': 'p_height', 'weight':'p_weight', 'age':'p_age'}, inplace=True)
+    # result = pd.concat([f, p], axis=1, join='inner')
+
+    combined = pd.merge(f, p[["pitcher", "p_last", "p_first", "p_height", "p_weight", "p_age", "throws"]], on='pitcher')
+    p.rename(columns={'pitcher': 'batter', "p_last":"b_last", "p_first":"b_first", "p_height":"b_height", "p_weight":"b_weight", "p_age":"b_age"}, inplace=True)
+    # result = pd.merge(result, p, on='batter')
+    combined = pd.merge(combined, p[["batter", "b_last", "b_first", "b_height", "b_weight", "b_age", "bats"]], on='batter')
+    combined.head()
+    return combined
+def labe_encoder_conversion(data,features):
+    from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+    return np.array([LabelEncoder().fit_transform(data[:,i]) for i in range(data.shape[1])])
+
+def one_hot_encoding_conversion(data,features_for_LE_and_OH, extra_features_for_OH,features_rest, label = "umpcall"):
+    from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+    OH_enc = OneHotEncoder(sparse = False)
+    LE_result = labe_encoder_conversion(data[features_for_LE_and_OH].fillna("-").as_matrix(),features_for_LE_and_OH)
+    LE_and_extra = np.append(LE_result.transpose(),data[extra_features_for_OH].fillna("-"),axis=1)
+    OH_result = OH_enc.fit_transform(LE_and_extra)
+    x = np.append(OH_result,data[features_rest],axis=1)
+    y = data[label].as_matrix()
+    return x,y
+def generate_data(train_years, test_years, fx_features_to_keep,\
+                                features_for_LE_and_OH,\
+                                extra_features_for_OH,\
+                                features_rest,\
+                                base_dir = "Data/",\
+                                player_filename = "MLB_Players.csv",\
+                                label = "umpcall",\
+                                filename = "data.pickle",
+                                post_season = True):
+
+    train_regular_season = [base_dir+"MLB_201{0}/MLB_PitchFX_201{0}_RegularSeason.csv".format(i) for i in train_years]
+    train_post_season = [base_dir+"MLB_201{0}/MLB_PitchFX_201{0}_RegularSeason.csv".format(i) for i in train_years]
+
+    test_regular_season = [base_dir+"MLB_201{0}/MLB_PitchFX_201{0}_RegularSeason.csv".format(i) for i in test_years]
+    test_post_season = [base_dir+"MLB_201{0}/MLB_PitchFX_201{0}_RegularSeason.csv".format(i) for i in test_years]
+
+    if post_season:
+        train_data = read_and_combine_data(train_regular_season+train_post_season,fx_features_to_keep)
+        test_data = read_and_combine_data(train_regular_season+test_post_season,fx_features_to_keep)
+    else:
+        train_data = read_and_combine_data(train_regular_season,fx_features_to_keep)
+        test_data = read_and_combine_data(train_regular_season,fx_features_to_keep)
+    player = age_conversion(pd.read_csv(base_dir+player_filename))
+
+    train_data = union_batter_pitcher(player,train_data)
+    test_data = union_batter_pitcher(player,test_data)
+
+    train_sz = train_data.shape[0]
+    train_test = pd.concat([train_data,test_data])
+    print("Start One-hot encoding")
+    x,y = one_hot_encoding_conversion(train_test,
+                                        features_for_LE_and_OH,
+                                        extra_features_for_OH,
+                                        features_rest)
+    print("Start writing data to {}".format(base_dir+filename))
+    save_as_picke({"x_train":x[:train_sz,:],"y_train":y[:train_sz],
+                    "x_test":x[train_sz:,:],"y_test":y[train_sz:]},
+                    base_dir+filename)
+    
+
+def main():
+    fx_features_to_keep = ["pitcher","batter", "balls","strikes","pitch_count","inning","side", "umpcall"]
+    train_year = [2,3]
+    test_year = [6]
+    player_filename = "MLB_Players.csv"
+    features_for_LE_and_OH = ["pitcher","batter","side","throws","bats"]
+    extra_features_for_OH = ["inning"]      
+    features_rest = ["pitch_count","balls","strikes"]
+    filename = "{0}|{1}.pickle".format(str(train_year),str(train_year))
+
+    generate_data(train_year,test_year,fx_features_to_keep,
+            features_for_LE_and_OH,
+            extra_features_for_OH,
+            features_rest,filename=filename)
+    
+    # to read data from pickle
+    x_train, y_train, x_test, y_test =  load_problem(filename)
+    
+if __name__ == '__main__':
+  main()
+
